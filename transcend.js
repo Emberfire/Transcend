a912a17febf41033685b417ee0ff4f53fec7ad0c
Emberfire <petar6464@gmail.com>
2019-12-05 00:42:59 +0200

    Context menu now never expands beyond viewport, is more responsive and supports adding elements to it.

--- transcend.js
+++ transcend.js
@@ -1,78 +1,136 @@
-class ContextMenu {
-  #contextMenu;
-  #expanded;
-  #expandedHeight;
-  #expandedWidth;
+class Transcend {
+  constructor () {
+    this.contextMenu = new ContextMenu(document.querySelector(".test1"));
+  }
+}
 
-  constructor() {
-    this.#contextMenu = document.querySelector(".context-menu");
-    this.#expanded = false;
+class ContextMenu {
+
+  constructor (parentElement) {
+    if (parentElement && typeof parentElement === "object") {
+      this.parentElement = parentElement;
+    } else {
+      this.parentElement = window;
+    }
+
+    this.contextMenu = document.querySelector(".context-menu");
+    this.expanded = false;
+    this.scrollable = false;
 
     this.calculateScales();
     this.setEventListeners();
   }
 
-  calculateScales() {
-    let temp = this.#contextMenu.style.height;
-    this.#contextMenu.style.height = "auto";
+  calculateScales () {
+    let temp = this.contextMenu.style.height;
+    this.contextMenu.style.height = "auto";
 
-    let dimensions = this.#contextMenu.getBoundingClientRect()
-    this.#expandedHeight = dimensions.height;
-    this.#expandedWidth = dimensions.width;
-
-    this.#contextMenu.style.height = temp;
-  }
-
-  calculatePosition(event) {
-    let x;
-    let y;
-    if (event.pageY >= (window.innerHeight / 2)) {
-      y = `${event.pageY - this.#expandedHeight - 17}px`;
-    } else {
-      y = `${event.pageY - 17}px`;
+    let dimensions = this.contextMenu.getBoundingClientRect();
+    if (window.innerHeight - 10 < dimensions.height) {
+      dimensions.height = window.innerHeight - 15;
+      this.contextMenu.style.overflowY = "scroll";
+      this.scrollable = true;
     }
 
-    if (event.pageX + this.#expandedWidth + 10 >= window.innerWidth) {
-      x = `${event.pageX - ((event.pageX + this.#expandedWidth) - window.innerWidth) - 10}px`
+    this.expandedHeight = dimensions.height;
+    this.expandedWidth = dimensions.width;
+
+    if (temp) {
+      this.contextMenu.style.height = temp;
     } else {
-      x = `${event.pageX}px`;
+      this.contextMenu.style.removeProperty("height");
+    }
+  }
+
+  calculatePosition (event) {
+    let x;
+    let y;
+
+    // Open menu upwards if pointer is below half of the screen
+    if (event.pageY >= (window.innerHeight / 2)) {
+      let overShootUp = Math.min(0, event.pageY - this.expandedHeight)
+
+      y = `${event.pageY - overShootUp - this.expandedHeight - 15}px`;
+    } else {
+      // If the dropdown would overshoot the viewport and remain partially hidden, offset it up
+      let overShootDown = Math.max(0, (event.pageY + this.expandedHeight) - window.innerHeight);
+
+      y = `${event.pageY - overShootDown - 15}px`;
+    }
+
+    if (event.pageX + this.expandedWidth + 10 >= window.innerWidth) {
+      x = `${event.pageX - ((event.pageX + this.expandedWidth) - window.innerWidth) - 10}px`
+    } else {
+      x = `${event.pageX + 3}px`;
     }
 
     return { x, y };
   }
 
   expand (event) {
-    this.#contextMenu.classList.remove("context-menu-expanded");
+    this.contextMenu.classList.remove("context-menu-expanded");
+
     let { x, y } = this.calculatePosition(event);
-    this.#contextMenu.style.left = x;
-    this.#contextMenu.style.top = y;
+    this.contextMenu.style.left = x;
+    this.contextMenu.style.top = y;
 
-    this.#contextMenu.style.height = `${this.#expandedHeight}px`;
-    this.#contextMenu.classList.add("context-menu-expanded");
+    if (this.scrollable) {
+      this.contextMenu.scrollTo(0, 0);
+    }
 
-    this.#expanded = true;
+    this.contextMenu.style.height = `${this.expandedHeight}px`;
+    this.contextMenu.classList.add("context-menu-expanded");
+
+    this.expanded = true;
   }
 
   collapse () {
-    this.#contextMenu.style.height = "0px";
-    this.#contextMenu.classList.remove("context-menu-expanded");
+    //this.contextMenu.style.height = "0px";
+    this.contextMenu.style.removeProperty("height");
+    this.contextMenu.classList.remove("context-menu-expanded");
 
-    this.#expanded = false;
+    this.expanded = false;
+  }
+
+  add (element) {
+    if (typeof element == "object") {
+      this.contextMenu.appendChild(element);
+      this.calculateScales();
+    } else if (typeof element == "string") {
+      this.contextMenu.innerHTML += element;
+      this.calculateScales();
+    }
+
+    if (this.expanded) {
+      this.contextMenu.style.height = `${this.expandedHeight}px`;
+    }
+  }
+
+  remove (element) {
+
   }
 
   setEventListeners() {
     let self = this;
-    window.addEventListener("contextmenu", function (event) {
+
+    this.parentElement.addEventListener("contextmenu", function (event) {
       event.preventDefault();
-        self.expand(event);
+
+      self.collapse();
+      self.expand(event);
     });
 
     window.addEventListener("click", function (event) {
-      if (self.#expanded) {
+      if (self.expanded) {
         self.collapse();
       }
+    });
+
+    // Don't close the context menu if clicked inside it
+    this.contextMenu.addEventListener("click", function (event) {
+      event.stopPropagation();
     });
   }
 }
 
-let a = new ContextMenu();
+let ts = new Transcend();
